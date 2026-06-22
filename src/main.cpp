c31c5afc0830100bb117d83c02d4ffcd89727bfb
Peter Gilbert <petergilbert360@gmail.com>
2026-06-22 23:16:34 +0900

    refactor main loop into smaller functions + work on framerate limit + counter

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,42 @@ public:
         SDL_Quit();
     }
 
-    bool bRunning = true;
-
     void run() {
         window = SDL_CreateWindow("yellowtail.", 1280, 720, 0);
         if (!window) {
             std::cout << "Error creating SDL window!" << std::endl;
         }
+        mainLoop();
+    }
+
+    void mainLoop() {
+        Uint64 fps = 0;
+        Uint64 lastTime = 0;
         while (bRunning) {
+            const Uint64 currentTick = SDL_GetTicks();
             tick(0.0f);
+            fps++;
+            Uint64 deltaTime = SDL_GetTicks() - currentTick;
+
+            // framerate lock
+            // SDL_Delay(16);
+            // 1 sec == 1000ms and 1000ms/60frames = 16ms
+            // we delay the target amount - the delta time
+            if (framerateLock > 0) {
+                Sint64 targetMs = 1000/framerateLock;
+                targetMs -= static_cast<Sint64>(deltaTime);
+                if (targetMs > 0) {
+                    SDL_Delay(targetMs);
+                }
+            }
+
+            // this is once a second (so we can find our frames per second)
+            if (currentTick > lastTime + 1000) {
+                lastTime = currentTick;
+                std::string debugString = "Current FPS: " + std::to_string(fps) + " Delta Time: " + std::to_string(deltaTime);
+                SDL_SetWindowTitle(window, debugString.c_str());
+                fps = 0;
+            }
         }
     }
 
@@ -32,7 +59,14 @@ public:
         bRunning = false;
     }
 
+    // Advance a frame (one iteration in the main loop)
     void tick(float deltaTime) {
+        inputTick();
+        updateTick();
+        renderTick();
+    }
+
+    void inputTick() {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -47,7 +81,15 @@ public:
         }
     }
 
-    void handleInput(SDL_KeyboardEvent& keyboard_event) {
+    void updateTick() {
+
+    }
+
+    void renderTick() {
+
+    }
+
+    void handleInput(const SDL_KeyboardEvent& keyboard_event) {
         if (keyboard_event.key == SDLK_ESCAPE) {
             quit();
         }
@@ -55,11 +97,16 @@ public:
 
 protected:
     SDL_Window* window = nullptr;
+    bool bRunning = true;
+
+    // locks the framerate if greater than 0
+    int framerateLock = 30;
 };
 
 
-
-int main(int /*argc*/, char** /*argv*/) {
+// entry point
+int main(int argc, char* argv[]) {
+    std::cout << "Starting yellowtail process." << std::endl;
     Yellowtail Game;
     Game.run();
     std::cout << "Ending yellowtail process." << std::endl;
