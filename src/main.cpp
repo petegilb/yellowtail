3f08889094f578e4ab67f7bbf6646cfbf5e53f6b
Peter Gilbert <petergilbert360@gmail.com>
2026-06-24 19:42:36 +0900

    print clear color + prepare to draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,36 @@ public:
         if (!SDL_Init(SDL_INIT_VIDEO)) {
             std::cout << "Error starting SDL!" << std::endl;
         }
+        InitializeAssetLoader();
     }
     ~Yellowtail() {
         std::cout << "Ending yellowtail..." << std::endl;
+        if (device && window) SDL_ReleaseWindowFromGPUDevice(device, window);
+        if (device) SDL_DestroyGPUDevice(device);
         if (window) SDL_DestroyWindow(window);
         SDL_Quit();
     }
 
-    void run() {
+    int run() {
+        device = SDL_CreateGPUDevice(
+            SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_MSL,
+            true,
+            nullptr);
+        if (device == nullptr){
+            SDL_Log("CreateDevice failed");
+            return -1;
+        }
         window = SDL_CreateWindow("yellowtail.", 1280, 720, 0);
         if (!window) {
             std::cout << "Error creating SDL window!" << std::endl;
+            return -1;
+        }
+        if (!SDL_ClaimWindowForGPUDevice(device, window)){
+            SDL_Log("ClaimWindow failed");
+            return -1;
         }
         mainLoop();
+        return 0;
     }
 
     void mainLoop() {
@@ -90,8 +107,36 @@ public:
 
     }
 
-    void renderTick() {
+    int renderTick() {
+        // command buffer of commands to be executed by SDL3_gpu
+        // does not need to be freed and can only be used on the thread in which it is created (render thread?)
+        SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(device);
+        if (cmdbuf == nullptr){
+            SDL_Log("AcquireGPUCommandBuffer failed: %s", SDL_GetError());
+            return -1;
+        }
+        // a swapchain is a queue of framebuffers (or textures) that are swapped to the screen one after another
+        // allow rendering into one texture while another is being displayed
+        // help avoid tearing, flickering, and stuttering
+        SDL_GPUTexture* swapchainTexture;
+        if (!SDL_WaitAndAcquireGPUSwapchainTexture(cmdbuf, window, &swapchainTexture, nullptr, nullptr)) {
+            SDL_Log("WaitAndAcquireGPUSwapchainTexture failed: %s", SDL_GetError());
+            return -1;
+        }
+        if (swapchainTexture != nullptr)
+        {
+            SDL_GPUColorTargetInfo colorTargetInfo = { 0 };
+            colorTargetInfo.texture = swapchainTexture;
+            colorTargetInfo.clear_color = (SDL_FColor){ 0.3f, 0.4f, 0.5f, 1.0f };
+            colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+            colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
 
+            SDL_GPURenderPass* renderPass = SDL_BeginGPURenderPass(cmdbuf, &colorTargetInfo, 1, nullptr);
+            SDL_EndGPURenderPass(renderPass);
+        }
+
+        SDL_SubmitGPUCommandBuffer(cmdbuf);
+        return 0;
     }
 
     void handleInput(const SDL_KeyboardEvent& keyboard_event) {
@@ -100,9 +145,105 @@ public:
         }
     }
 
+    void drawTriangle() {
+        SDL_GPUShader* vertexShader = loadShader(device, "Triangle.vert.hlsl", 0, 0, 0, 0);
+        if (vertexShader == nullptr)
+        {
+            SDL_Log("Failed to create vertex shader!");
+        }
+
+        SDL_GPUShader* fragmentShader = loadShader(device, "SolidColor.frag.hlsl", 0, 0, 0, 0);
+        if (fragmentShader == nullptr)
+        {
+            SDL_Log("Failed to create fragment shader!");
+        }
+    }
+
+    SDL_GPUShader* loadShader(
+        SDL_GPUDevice* inDevice,
+        const char* shaderFilename,
+        Uint32 samplerCount,
+        Uint32 uniformBufferCount,
+        Uint32 storageBufferCount,
+        Uint32 storageTextureCount
+    ) {
+        SDL_GPUShaderStage stage;
+        if (SDL_strstr(shaderFilename, ".vert"))
+        {
+            stage = SDL_GPU_SHADERSTAGE_VERTEX;
+        }
+        else if (SDL_strstr(shaderFilename, ".frag"))
+        {
+            stage = SDL_GPU_SHADERSTAGE_FRAGMENT;
+        }
+        else
+        {
+            SDL_Log("Invalid shader stage!");
+            return NULL;
+        }
+
+        char fullPath[256];
+        SDL_GPUShaderFormat backendFormats = SDL_GetGPUShaderFormats(inDevice);
+        SDL_GPUShaderFormat format = SDL_GPU_SHADERFORMAT_INVALID;
+        const char *entrypoint;
+
+        if (backendFormats & SDL_GPU_SHADERFORMAT_SPIRV) {
+            SDL_snprintf(fullPath, sizeof(fullPath), "%s%s.spv", BasePath, shaderFilename);
+            format = SDL_GPU_SHADERFORMAT_SPIRV;
+            entrypoint = "main";
+        } else if (backendFormats & SDL_GPU_SHADERFORMAT_MSL) {
+            SDL_snprintf(fullPath, sizeof(fullPath), "%s%s.msl", BasePath, shaderFilename);
+            format = SDL_GPU_SHADERFORMAT_MSL;
+            entrypoint = "main0";
+        } else if (backendFormats & SDL_GPU_SHADERFORMAT_DXIL) {
+            SDL_snprintf(fullPath, sizeof(fullPath), "%s%s.dxil", BasePath, shaderFilename);
+            format = SDL_GPU_SHADERFORMAT_DXIL;
+            entrypoint = "main";
+        } else {
+            SDL_Log("%s", "Unrecognized backend shader format!");
+            return NULL;
+        }
+
+        size_t codeSize;
+        void* code = SDL_LoadFile(fullPath, &codeSize);
+        if (code == NULL)
+        {
+            SDL_Log("Failed to load shader from disk! %s", fullPath);
+            return NULL;
+        }
+
+        SDL_GPUShaderCreateInfo shaderInfo = {
+            .code = (const Uint8 *)code,
+            .code_size = codeSize,
+            .entrypoint = entrypoint,
+            .format = format,
+            .stage = stage,
+            .num_samplers = samplerCount,
+            .num_uniform_buffers = uniformBufferCount,
+            .num_storage_buffers = storageBufferCount,
+            .num_storage_textures = storageTextureCount
+        };
+        SDL_GPUShader* shader = SDL_CreateGPUShader(inDevice, &shaderInfo);
+        if (shader == NULL)
+        {
+            SDL_Log("Failed to create shader!");
+            SDL_free(code);
+            return NULL;
+        }
+
+        SDL_free(code);
+        return shader;
+    }
+
+    void InitializeAssetLoader(){
+        BasePath = SDL_GetBasePath();
+    }
+
 protected:
     SDL_Window* window = nullptr;
     bool bRunning = true;
+    SDL_GPUDevice* device = nullptr;
+    const char* BasePath = nullptr;
 
     // locks the framerate if greater than 0
     int framerateLock = 60;
@@ -113,7 +254,5 @@ protected:
 int main(int argc, char* argv[]) {
     std::cout << "Starting yellowtail process." << std::endl;
     Yellowtail Game;
-    Game.run();
-    std::cout << "Ending yellowtail process." << std::endl;
-    return 0;
+    return Game.run();
 }
